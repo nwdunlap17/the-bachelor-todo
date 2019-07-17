c69a4808be39a9c2471269d1180d384024158f6c
Nick Dunlap <nwdunlap17@gmail.com>
2019-07-17 13:27:23 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,46 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(" ")[0]
+    end
+  end
+end
+
+def get_contestants(data)
+  contestants = []
+  data.each_key do |season|
+    contestants << data[season]
+  end
+  return contestants.flatten()
+end
+
+def find_contestant(data)
+  contestants = get_contestants(data)
+  output = []
+  contestants.each do |person|
+    if person[yield[0]] == yield[1]
+      output << person
+    end
+  end
+  return output
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  return find_contestant(data){["occupation",occupation]}[0]["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  return find_contestant(data){["hometown",hometown]}.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  return find_contestant(data){["hometown",hometown]}[0]["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total = 0.0
+  data[season].each do |person|
+    total += person["age"].to_i
+  end
+  return (total / data[season].length).round(0)
 end
